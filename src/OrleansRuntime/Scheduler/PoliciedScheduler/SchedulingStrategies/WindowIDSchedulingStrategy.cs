afaf9959d3af9e56f97fe901271d9d33443ae7f4
flint-stone <sharonxu65@gmail.com>
2018-06-01 11:35:45 -0500

    clean up

--- src/OrleansRuntime/Scheduler/PoliciedScheduler/SchedulingStrategies/WindowIDSchedulingStrategy.cs
+++ src/OrleansRuntime/Scheduler/PoliciedScheduler/SchedulingStrategies/WindowIDSchedulingStrategy.cs
@@ -11,10 +11,6 @@ namespace Orleans.Runtime.Scheduler.PoliciedScheduler.SchedulingStrategies
         private readonly LoggerImpl logger = LogManager.GetLogger("Scheduler.PoliciedScheduler.SchedulingStrategies", LoggerType.Runtime);
 
         #region Tenancies
-
-        private Dictionary<short, Tuple<ulong, HashSet<ulong>>> tenants;
-        private Dictionary<short, long> timeLimitsOnTenants;
-        private Dictionary<WorkItemGroup, FixedSizedQueue<double>> tenantStatCounters;
         private const int MaximumStatCounterSize = 100;
         // TODO: FIX LATER
         private int statCollectionCounter = 100;
@@ -29,34 +25,15 @@ namespace Orleans.Runtime.Scheduler.PoliciedScheduler.SchedulingStrategies
             return SchedulerConstants.DEFAULT_PRIORITY;
         }
 
-        public void Initialization()
-        {
-            tenants = new Dictionary<short, Tuple<ulong, HashSet<ulong>>>();
-            timeLimitsOnTenants = new Dictionary<short, long>();
-            tenantStatCounters = new Dictionary<WorkItemGroup, FixedSizedQueue<double>>();
-        }
+        public void Initialization() { }
 
         public void OnWorkItemInsert(IWorkItem workItem, WorkItemGroup wig) { }
 
         public void OnReceivingControllerInstructions(IWorkItem workItem, ISchedulingContext context)
         {
             // Populate Topology info
-            var controllerContext = ((InvokeWorkItem)workItem).ControllerContext;
 
-            ulong controllerId = ((InvokeWorkItem)workItem).SourceActivation.Grain.Key.N1;
             var schedulingContext = context as SchedulingContext;
-            if (tenants.ContainsKey(controllerContext.AppId))
-            {
-                tenants[controllerContext.AppId].Item2.Add(schedulingContext.Activation.Grain.Key.N1);
-            }
-            else
-            {
-                // Initialize entries in *ALL* per-dataflow maps
-                tenants.Add(controllerContext.AppId, new Tuple<ulong, HashSet<ulong>>(controllerId, new HashSet<ulong>()));
-                timeLimitsOnTenants.Add(controllerContext.AppId, controllerContext.Time);
-
-                tenants[controllerContext.AppId].Item2.Add(schedulingContext.Activation.Grain.Key.N1);
-            }
             var wig = Scheduler.GetWorkItemGroup(schedulingContext);
             if (wig == null)
             {
@@ -64,10 +41,6 @@ namespace Orleans.Runtime.Scheduler.PoliciedScheduler.SchedulingStrategies
                     "WorkItem {0} on context {1} does not match a work item group", workItem, context);
                 logger.Error(ErrorCode.SchedulerQueueWorkItemWrongCall, error);
                 throw new InvalidOperationException(error);
-            }
-            if (!tenantStatCounters.ContainsKey(wig))
-            {
-                tenantStatCounters.Add(wig, new FixedSizedQueue<double>(MaximumStatCounterSize));
             }
         }
 
