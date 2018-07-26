b6e7ec99de8b50e04d4feebda236410e381c6bbf
flint-stone <sharonxu65@gmail.com>
2018-07-26 14:13:32 -0500

    clean up

--- src/OrleansRuntime/Scheduler/ControllerContext.cs
+++ src/OrleansRuntime/Scheduler/ControllerContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Orleans.Runtime.Scheduler
 {
@@ -13,31 +14,16 @@ namespace Orleans.Runtime.Scheduler
 
         internal HashSet<ActivationAddress> ActivationSeen { get; set; }
 
-        public ControllerContext(short appId, long time, ulong controllerKey)
+        public ControllerContext(short appId, long time, ulong controllerKey, Dictionary<ulong, long> grainKeysToDurations)
         {
             AppId = appId;
             Time = time;
             ControllerKey = controllerKey;
-            windowedKey = new Dictionary<ulong, long>();
-            windowedKey.Add(281513631416320, 100000000);
-            windowedKey.Add(844463584837632, 100000000);
-            windowedKey.Add(562988608126976, 100000000);
-            windowedKey.Add(2251838468390912, 100000000);
-            windowedKey.Add(1407413538258944, 100000000);
-            windowedKey.Add(1125938561548288, 100000000);
-            windowedKey.Add(1688888514969600, 100000000);
-            windowedKey.Add(1970363491680256, 100000000);
-            windowedKey.Add(3377738375233536, 100000000);
-            windowedKey.Add(3096263398522880, 100000000);
-            windowedKey.Add(2533313445101568, 100000000);
-            windowedKey.Add(4222163305365504, 100000000);
-            windowedKey.Add(2814788421812224, 100000000);
-            windowedKey.Add(3659213351944192, 100000000);
-            windowedKey.Add(3940688328654848, 100000000);
-            windowedKey.Add(4503638282076160, 100000000);
+            windowedKey = grainKeysToDurations
+                .Where(kv => kv.Value!=1L)
+                .ToDictionary(kv=>kv.Key, kv=>kv.Value);
 
             ActivationSeen = new HashSet<ActivationAddress>();
-
         }
     }
 }
