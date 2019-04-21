a32cb422261d13fb7b701de2243c32c32bc67c1a
Le Xu <sharonxu65@gmail.com>
2019-04-21 00:06:38 +0000

    change granularity size

--- src/OrleansRuntime/Scheduler/SchedulerUtility/SchedulerConstants.cs
+++ src/OrleansRuntime/Scheduler/SchedulerUtility/SchedulerConstants.cs
@@ -15,7 +15,7 @@ namespace Orleans.Runtime.Scheduler.SchedulerUtility
         internal const int MEASUREMENT_PERIOD_WORKITEM_COUNT = 1;
         internal const int STATS_COUNTER_QUEUE_SIZE = 100;
         internal const int DEFAULT_TASK_TRACKING_ID = -1;
-        internal const int PRIORITY_GRANULARITY_TICKS = 10000;
+        internal const int PRIORITY_GRANULARITY_TICKS = 1;
 
         internal static int SCHEDULING_QUANTUM_MINIMUM_MILLIS = 50;
         // internal const long TASK_LENGTH_SAMPLING_INTERVAL
